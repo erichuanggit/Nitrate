5d305569eb43b5db77855e6b48bfa71623d0051d
Xuqing Kuang <xuqingkuang@gmail.com>
2010-10-14 18:51:45 +0800

    Fixed new admin URL

--- trunk/nitrate/tcms/urls.py
+++ trunk/nitrate/tcms/urls.py
@@ -36,7 +36,7 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
     
     # Index and static zone
     (r'^$', 'tcms.core.views.index'),
