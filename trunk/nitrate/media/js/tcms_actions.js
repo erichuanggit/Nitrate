ac5699b9ee38deb6fe4539580d30f16a8a494ef0
Xuqing Kuang <xuqingkuang@gmail.com>
2010-10-15 17:12:40 +0800

    Completed feature #634157 - Preselect product when adding new build

--- trunk/nitrate/media/js/tcms_actions.js
+++ trunk/nitrate/media/js/tcms_actions.js
@@ -1217,3 +1217,25 @@ function reloadWindow()
 {
     window.location.reload(true);
 }
+
+// Enhanced from showAddAnotherPopup in RelatedObjectLookups.js for Admin
+function popupAddAnotherWindow(triggeringLink, parameters)
+{
+    var name = triggeringLink.id.replace(/^add_/, '');
+    name = id_to_windowname(name);
+    href = triggeringLink.href
+    if (href.indexOf('?') == -1) {
+        href += '?_popup=1';
+    } else {
+        href  += '&_popup=1';
+    }
+    
+    // IMPOROMENT: Add parameters.
+    // FIXME: Add multiple parameters here
+    if (parameters)
+        href += '&' + parameters + '=' + $F('id_' + parameters);
+    
+    var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+    win.focus();
+    return false;
+}
