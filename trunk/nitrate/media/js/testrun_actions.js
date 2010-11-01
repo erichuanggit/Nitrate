45acb9b57acb90d6adc1f87ce9da98ed08df7412
June Zhang <junzhang@redhat.com>
2010-11-01 10:24:16 +0800

    Finished the front-code of clone multiple

--- trunk/nitrate/media/js/testrun_actions.js
+++ trunk/nitrate/media/js/testrun_actions.js
@@ -12,16 +12,19 @@ Nitrate.TestRuns.List.on_load = function()
     bind_version_selector_to_product(true, $('id_product'));
     bind_build_selector_to_product(true, $('id_product'));
     
-	$('relativeSearchOption_case').observe('click', function(e){
-		if($('relativeSearch_case').getStyle('display') == 'none'){
-			Effect.BlindDown('relativeSearch_case',{ duration: 0.5 });
-			this.className = 'up'
-		} else {
-			Effect.BlindUp('relativeSearch_case',{ duration: 0.5 });
-			this.className = 'down'
-		}
-	})
-	
+    $('relativeSearchOption_case').observe('click', function(e){
+        if($('relativeSearch_case').getStyle('display') == 'none'){
+            Effect.BlindDown('relativeSearch_case',{ duration: 0.5 });
+            this.className = 'up'
+        } else {
+            Effect.BlindUp('relativeSearch_case',{ duration: 0.5 });
+            this.className = 'down'
+        }
+    })
+    
+    $('id_check_all_runs').observe('click',function(e){
+        clickedSelectAll(this, 'testruns_table', 'run')
+    })
     if($('testruns_table')) {
         TableKit.Sortable.init('testruns_table',
         {
