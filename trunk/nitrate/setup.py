dde5ba2e16ae57828fc616c9c77847962ddff93f
Yuguang Wang <yuwang@redhat.com>
2011-01-25 20:24:20 +0800

    Build package for nitrate 3.3-1

--- trunk/nitrate/setup.py
+++ trunk/nitrate/setup.py
@@ -23,7 +23,7 @@ import tcms
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'Nitrate'
-PACKAGE_VER = '3.2.0'
+PACKAGE_VER = '3.3.0'
 PACKAGE_DESC = 'Test Case Management System'
 PACKAGE_URL = 'https://fedorahosted.org/nitrate/browser/trunk/nitrate'
 
