d0cb024157b6040270b22dfa27aa66fb3ecc1034
Yuguang Wang <yuwang@redhat.com>
2011-02-23 11:39:42 +0800

    Modified the report tags percentage.

--- trunk/nitrate/tcms/core/templatetags/report_tags.py
+++ trunk/nitrate/tcms/core/templatetags/report_tags.py
@@ -6,6 +6,6 @@ register = template.Library()
 @register.filter(name = 'percentage')
 def percentage(fraction, population):
     try:
-        return "%.2f%%" % ((float(fraction) / float(population)) * 100)
+        return "%.1f%%" % ((float(fraction) / float(population)) * 100)
     except ZeroDivisionError, Exception:
         return '0%'
