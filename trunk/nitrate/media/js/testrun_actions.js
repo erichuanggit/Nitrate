3a777ee21c82b2fa1d92cd00a8cd6e1790f13a33
chaobin <cbtchn@gmail.com>
2011-07-07 14:53:48 +0800

    FIX: #707455 Using ID instead of relative DOM.

--- trunk/nitrate/media/js/testrun_actions.js
+++ trunk/nitrate/media/js/testrun_actions.js
@@ -448,7 +448,7 @@ function taggleSortCaseRun(event)
             t.checked = true;
             t.disabled = false;
         });
-        postToURL('ordercaserun/', serializeCaseRunFromInputList(element.up(2).next().next(), 'case_run'), 'get');
+        postToURL('ordercaserun/', serializeCaseRunFromInputList('id_table_cases', 'case_run'), 'get');
     }
 }
 function selectcase(){
