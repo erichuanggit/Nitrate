cfbb4f1c708da78acb3b249e6e4bab7f8f96b382
chaobin <cbtchn@gmail.com>
2011-07-08 17:03:33 +0800

    DOC: Amelioration on the script that updates testcaseplan table.

--- trunk/nitrate/contrib/tools/update_sortkey.py
+++ trunk/nitrate/contrib/tools/update_sortkey.py
@@ -5,8 +5,13 @@ import os
 TCMS_SETTINGS_MODULE = 'tcms.product_settings'
 os.environ['DJANGO_SETTINGS_MODULE'] = TCMS_SETTINGS_MODULE
 
+from django.db import models
+
 from tcms.testcases.models import TestCasePlan, TestCase
 from tcms.testplans.models import TestPlan
+
+_field = models.IntegerField(max_length=11, null=True, blank=True)
+_field.contribute_to_class(TestCasePlan, 'sortkey')
 
 def update_sortkey():
     for tc in TestCase.objects.all():
