11ce4168a26ae2d3369bb87354dfb836c8c1cc68
sdeng <sdeng@redhat.com>
2011-08-10 11:24:22 +0800

    adding deletion of testrun,testcase,testplan

--- trunk/nitrate/tcms/testplans/admin.py
+++ trunk/nitrate/tcms/testplans/admin.py
@@ -18,9 +18,20 @@
 
 from django.contrib import admin
 from tcms.testplans.models import TestPlanType
+from tcms.testplans.models import TestPlan
 
 class TestPlanTypeAdmin(admin.ModelAdmin):
     search_fields = (('name',))
     list_display = ('id', 'name', 'description')
 
+class TestPlanAdmin(admin.ModelAdmin):
+    #fieldsets=[
+#(None,{'fields':['name']}),
+#('TestPlan Information',{'fields':['create_date']}),
+    search_fields =(('name',))
+    list_filter=['owner','create_date']
+    list_display=('name','create_date','owner','author','type')
+#]
+
 admin.site.register(TestPlanType, TestPlanTypeAdmin)
+admin.site.register(TestPlan,TestPlanAdmin)
