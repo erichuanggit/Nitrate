7144f0e98d998ebd7883672765c5a4be0d4211ba
Chenxiong Qi <cqi@chen-rhlaptop.(none)>
2011-12-19 13:18:43 +0800

    Write test for OutgoingMessage class
    Change-Id: I68ca2648d0f026b036dd9ddeffadf90f41d66b10

--- trunk/nitrate/tcms/plugins/message_bus/tests.py
+++ trunk/nitrate/tcms/plugins/message_bus/tests.py
@@ -15,3 +15,24 @@
 
 # Plugin unit testing
 
+import unittest
+
+from qpid.messaging import Message
+from tcms.plugins.message_bus.outgoing_message import OutgoingMessage
+
+class TestOutgoingMessage(unittest.TestCase):
+
+    def testOutgoingMessageShouldRepresentMessage(self):
+        raw_msg = 'hello world'
+        event_name = 'bugs.add'
+
+        o_msg = OutgoingMessage(raw_msg, event_name)
+
+        self.assertTrue(isinstance(o_msg, Message))
+        self.assertEqual(o_msg.content, raw_msg)
+        self.assertEqual(o_msg.subject, 'tcms.%s' % event_name)
+        self.assertTrue(hasattr(o_msg, 'properties'))
+
+if __name__ == '__main__':
+    unittest.main()
+
