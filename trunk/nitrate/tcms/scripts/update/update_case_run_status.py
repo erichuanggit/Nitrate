e224266871f313cd510f96948e150b5fa61ef6b0
Tang Chaobin <ctang@redhat.com>
2011-12-19 00:06:40 -0800

    REV: Removed the deprecated profiling call.
    Change-Id: I9f5cb5102409c9668bb952754c6ff7ff38c18c63

--- trunk/nitrate/tcms/scripts/update/update_case_run_status.py
+++ trunk/nitrate/tcms/scripts/update/update_case_run_status.py
@@ -49,7 +49,7 @@ def get_runs():
         transaction.commit_unless_managed()
 
 def main():
-    cProfile.runctx('get_runs()', globals(), locals())
+    get_runs()
 
 if __name__ == '__main__':
     main()
