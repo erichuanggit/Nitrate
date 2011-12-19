cb23ec2f066fb9705e4580803baabf42c15d60e2
Chenxiong Qi <cqi@chen-rhlaptop.(none)>
2011-12-19 13:21:06 +0800

    relayout the method's doc
    Change-Id: I9971c0b7c84d7d467fbe86fee6ef542a8c01cbf2

--- trunk/nitrate/tcms/plugins/message_bus/outgoing_message.py
+++ trunk/nitrate/tcms/plugins/message_bus/outgoing_message.py
@@ -9,14 +9,18 @@ class OutgoingMessage(Message):
 
     def __init__(self, raw_msg, event_name, *args, **kwargs):
         ''' Using extra two argument to initialize the outgoing message
-            raw_msg: the raw message, which is sent by via QPID broker
-            event_name: a name that is used to construct message's routing key.
+
+            - raw_msg: the raw message, which is sent by via QPID broker
+
+            - event_name: a name that is used to construct message's routing key.
                         This is an internal name mapping to:
                         testrun.create, testrun.progress, testrun.report, bugs.add, bugs.removal
                         There may be more than these in the future. Just following this naming rule is OK.
-            The rest of arguments are the qpid's Message's.
+
+            - The rest of arguments are the qpid's Message's.
         '''
 
+        # Message is not a new-style class :(
         Message.__init__(self, *args, **kwargs)
 
         self.content = raw_msg
