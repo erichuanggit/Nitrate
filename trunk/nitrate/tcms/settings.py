c11babef910a9d69253983e2e8479450dc3d1e33
Chenxiong Qi <cqi@redhat.com>
2012-02-22 15:19:07 +0800

    Move the XMLRPC's version to xmlrpc module and developers can get this version by calling Version.get method.
    Change-Id: Icf0a6c7dbe6433d18268e2c37a56628155fde8ea

--- trunk/nitrate/tcms/settings.py
+++ trunk/nitrate/tcms/settings.py
@@ -180,6 +180,7 @@ XMLRPC_METHODS = {
         ('tcms.xmlrpc.testplan', 'TestPlan'),
         ('tcms.xmlrpc.testrun', 'TestRun'),
         ('tcms.xmlrpc.user', 'User'),
+        ('tcms.xmlrpc.version', 'Version'),
     ),
 }
 
