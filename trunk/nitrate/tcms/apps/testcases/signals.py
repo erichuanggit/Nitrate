1cebac765874341d2dae5a0c8f8f8cd3e5be76b9
jianchen <jianchen@cj.(none)>
2012-03-08 11:00:38 +0800

    REV: refactoring the apps/testcases signal listen about testcase update/delete and remove watchers.py
    Change-Id: Id551d5c8bf9343e2d71950d10a6905b16ad29b98

--- trunk/nitrate/tcms/apps/testcases/signals.py
+++ trunk/nitrate/tcms/apps/testcases/signals.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# 
+#
 # Nitrate is copyright 2010 Red Hat, Inc.
-# 
+#
 # Nitrate is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
@@ -9,15 +9,16 @@
 # the hope that it will be useful, but WITHOUT ANY WARRANTY; without
 # even the implied warranties of TITLE, NON-INFRINGEMENT,
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# 
+#
 # The GPL text is available in the file COPYING that accompanies this
 # distribution and at <http://www.gnu.org/licenses>.
-# 
+#
 # Authors:
 #   Xuqing Kuang <xkuang@redhat.com>
 
 import threading
 import datetime
+from tcms.apps.testcases.helpers import email
 
 # Reference from
 # http://www.chrisdpratt.com/2008/02/16/signals-in-django-stuff-thats-not-documented-well/
@@ -30,7 +31,7 @@ class EditCaseNotifyThread(threading.Thread):
         self.request = request
         self.to = to
         threading.Thread.__init__(self)
-    
+
     def run(self):
         # The actual code we want to run
         txt = self.instance.latest_text()
@@ -47,3 +48,14 @@ class EditCaseNotifyThread(threading.Thread):
             to = self.to,
             request = self.request,
         )
+
+def on_case_save(sender, instance, created=False, **kwargs):
+    # case update and email
+    if not created:
+        if instance.emailing.notify_on_case_update:
+            email.email_case_update(instance)
+
+def on_case_delete(sender, instance, **kwags):
+    # case delete and email
+    if instance.emailing.notify_on_case_delete:
+        email.email_case_deletion(instance)
