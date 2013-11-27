4853a7458a053a8e8fc246b3ecf31f429f88169a
Chenxiong Qi <cqi@redhat.com>
2013-11-27 16:13:13 +0800

    REV: remove unnecessary file
    Change-Id: Iee81198098772517a9ddf6628c61102725f51895

--- trunk/nitrate/tcms/core/actions.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.core.exceptions import ObjectDoesNotExist
-
-from tcms.apps.testcases.views import get_selected_testcases
-from tcms.core.utils.mailto import mailto
